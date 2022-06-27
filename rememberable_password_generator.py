94c57bc5683c8f0d8a7e94ba775040cb9fdb3f1c
santitomatis <santiagotomatis06@gmail.com>
2022-06-26 23:54:52 -0300

    I'm trying to create the array with nouns with pandas

--- rememberable_password_generator.py
+++ rememberable_password_generator.py
@@ -1,27 +1,17 @@
+import random
+import pandas as pd
+
+def file_to_list(file):
+    rtn: object = []
+    file_object: object = open(file, "r")
+    rtn: object = file_object.read().splitlines()
+    file_object.close()
+    return list(filter(None, pd.unique(rtn).tolist())) # Remove Empty/Duplicates Values
+    pass
+
 def run():
-	a_file = open("nouns.txt", "r")
-
-	list_of_lists = []
-	for line in a_file:
-		stripped_line = line.strip()
-		line_list = stripped_line.split()
-		list_of_lists.append(line_list)
-
-	a_file.close()
-
-	j = 0
-	for i in list_of_lists:
-		auxiliar = str(list_of_lists[j])
-		print(auxiliar)
-		auxiliar.replace("[", "")
-		auxiliar.replace("]", "") 
-		print(auxiliar)
-		break
-		list_of_lists[j] = auxiliar
-		j = j + 1
-
-#test
-	print(list_of_lists)
+    data_from_file: object = file_to_list('nouns.txt') 
+    print(data_from_file)
 
 if __name__ == "__main__":
     run()
