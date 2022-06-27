c79cfa035632023c473b4e76c2aea0c4701a2932
santitomatis <santiagotomatis06@gmail.com>
2022-06-27 00:35:40 -0300

    still trying to remove ' from the array, I9ll try again tomorrow

--- rememberable_password_generator.py
+++ rememberable_password_generator.py
@@ -7,11 +7,21 @@ def file_to_list(file):
     rtn: object = file_object.read().splitlines()
     file_object.close()
     return list(filter(None, pd.unique(rtn).tolist())) # Remove Empty/Duplicates Values
-    pass
+
+def strip(list):
+	stripped_list = []
+	# print(len(stripped_list))
+	for i in range(len(list)):
+		assistant = list[i].replace("'", "")
+		stripped_list.append(assistant)
+		return stripped_list
+	
 
 def run():
-    data_from_file: object = file_to_list('nouns.txt') 
-    print(data_from_file)
+	nouns: object = file_to_list('nouns.txt') 
+	# print(nouns)
+	stripped_nouns: object = strip(nouns)
+	print(stripped_nouns)
 
 if __name__ == "__main__":
-    run()
+	run()
