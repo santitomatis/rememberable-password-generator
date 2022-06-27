dc1e052aba5043367bc81210e36fda6bacba1f95
santitomatis <santiagotomatis06@gmail.com>
2022-06-26 23:46:14 -0300

    This is the first version. I'm having trouble converting the nouns.txt file to an array

---
+++ rememberable_password_generator.py
@@ -0,0 +1,27 @@
+def run():
+	a_file = open("nouns.txt", "r")
+
+	list_of_lists = []
+	for line in a_file:
+		stripped_line = line.strip()
+		line_list = stripped_line.split()
+		list_of_lists.append(line_list)
+
+	a_file.close()
+
+	j = 0
+	for i in list_of_lists:
+		auxiliar = str(list_of_lists[j])
+		print(auxiliar)
+		auxiliar.replace("[", "")
+		auxiliar.replace("]", "") 
+		print(auxiliar)
+		break
+		list_of_lists[j] = auxiliar
+		j = j + 1
+
+
+	print(list_of_lists)
+
+if __name__ == "__main__":
+    run()
